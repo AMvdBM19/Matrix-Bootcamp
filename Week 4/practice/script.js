01dac801761b8a5dcf3e1a0868808ec73c6f8e0e
Andres Mendoza <andresmarcel19@hotmail.com>
2026-02-05 01:08:57 +0100

    practice

---
+++ Week 4/practice/script.js
@@ -0,0 +1,21 @@
+
+//DOM
+
+//Select HTML elements
+const firstName = document.getElementById("first-name");
+const lastName = document.getElementById("last-name");
+
+const addName = document.getElementById("submit");
+
+//Event for button
+
+// Create empty array
+
+//Check for NOT null values
+    //If empty, notify
+    //If value = TRUE
+        // Combine them to fullName
+        // Push to array
+            //loop through arrat
+            // for user print <li>
+            // Append <li> inside <ul>
