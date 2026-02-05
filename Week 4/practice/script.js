ad3783d408a947fcbbe107996d31d3cb2f60a24f
YMNL-IT Andrés van den Bos Mendoza <andres@yml.nl>
2026-02-05 12:48:05 +0100

    practice1

--- Week 4/practice/script.js
+++ Week 4/practice/script.js
@@ -5,17 +5,47 @@
 const firstName = document.getElementById("first-name");
 const lastName = document.getElementById("last-name");
 
+const myForm = document.getElementById("my-form");
 const addName = document.getElementById("submit");
 
+const userList = document.getElementById('user-list');
+
+// Declare empty array
+let users = [];
+
 //Event for button
+document.addEventListener("submit", function (e) {
+    // Create empty array
+    e.preventDefault();
 
-// Create empty array
+    //Check for NOT null values
+    if (!firstName.value || !lastName.value){
+        //If empty, notify
+        alert("Input values");
+        
 
-//Check for NOT null values
-    //If empty, notify
     //If value = TRUE
+    } else {
+
         // Combine them to fullName
+        let fullName = firstName.value + " " + lastName.value
+
+        // loop through array
+        function addLatestItem(arr) {
+            const li = document.createElement('li');
+            li.textContent = arr[arr.length - 1];
+            userList.appendChild(li);
+        }
+        
         // Push to array
-            //loop through arrat
-            // for user print <li>
-            // Append <li> inside <ul>
+        users.push(fullName);
+        addLatestItem(users);
+
+        alert("Okay");
+    }
+
+    // Empty form
+    myForm.reset()
+
+    console.log(users)
+});
